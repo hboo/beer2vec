e799d62204a73a8d9edf94c74135e0606eec4aaa
hboo <hboo@users.noreply.github.com>
2018-03-12 21:40:32 -0400

    Update beer2vec.py

--- beer2vec.py
+++ beer2vec.py
@@ -4,6 +4,7 @@ from itertools import izip
 import numpy as np
 
 class FileToSentence():
+	# generator which constructs sentences from a file of json words
 	def __init__(self, filename):
 		self.filename = filename
 
@@ -13,7 +14,9 @@ class FileToSentence():
 			yield ll
 
 def initialize_model(win=1000, ct=1, sz=100):
+	#build sentences from training data
 	sentences = FileToSentence('./training_sentences_20180121.txt')
+	#initialize and train model
 	model = word2vec.Word2Vec(sentences=sentences, size=sz, window=win, min_count=ct, workers=4, hs=1)
 	model.save('initial_w2v_20180121')
 	return model
@@ -21,7 +24,9 @@ def initialize_model(win=1000, ct=1, sz=100):
 @click.command()
 @click.option('--beer', prompt='Beer ')
 def make_suggestion(beer):
+	# load model from disk
 	model = word2vec.Word2Vec.load('initial_w2v')
+	# this method returns the beers with the highest cosine similarities
 	results = model.wv.most_similar(beer)
 	for suggestion in results:
 		info = [x.replace('_', ' ').encode('utf-8') for x in suggestion[0].split(':')]
@@ -38,18 +43,23 @@ def evaluate():
 			
 			def get_results(data):
 				try:
+					# predict
 					return model.wv.most_similar(positive=data)
 				except KeyError as e:
+					# the beer may not exist in the model because it was not commonly reviewed
 					not_found = e.message[6:-19]
 					if len(data) < 2:
-						return 'x'
+						# no recommendation was possible - no beers found in model
+						return ''
+					# unless the list is empty, remove the unrepresented beer and 
+					# try the prediction again with the remaining beers
 					data.remove(not_found)
 					get_results(data)
 
 			results = get_results(test_line.split(' ')[:-1])
-			#print results
 
 			if results:
+				# count how many recommendations were represented in the same user's history
 				x +=1
 				for suggestion in results:
 					if suggestion[0].encode('utf-8') in train_line:
@@ -58,15 +68,14 @@ def evaluate():
 			else:
 				y += 1
 
-		#print 'not found:{}'.format(str(y))
-		#print 'found with 5:{}'.format(str(x))
 		return matches
 
 if __name__ == '__main__':
-	#make_suggestion()
-	#initialize_model()
-	#evaluate()
-
+	make_suggestion()
+	initialize_model()
+	evaluate()
+	
+	# use grid search on these three params
 	windows = [1000, 500, 100, 50, 10, 1]
 	min_count = [1, 2, 3, 4, 5]
 	size = [5, 10, 50, 100]
